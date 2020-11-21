779a1b89645fa1854775b4259c1496e7c1458d19
shockliang <shockwater@gmail.com>
2020-11-22 02:05:29 +0800

    Relocating shared code.

---
+++ src/errors/database-connection-error.ts
@@ -0,0 +1,16 @@
+import {CustomError} from "./custom-error";
+
+export class DatabaseConnectionError extends CustomError {
+    statusCode = 500;
+    reason = 'Error to connecting to database';
+
+    constructor() {
+        super('Error to connecting to database');
+        Object.setPrototypeOf(this, DatabaseConnectionError.prototype);
+    }
+
+    serializeErrors() {
+        return [
+            {message: this.reason}]
+    };
+}
