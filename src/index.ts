2153dee6fe790d5276c8c3f3f6922af0a5e715a2
shockliang <shockwater@gmail.com>
2021-01-20 20:11:45 +0800

    Update common lib

--- src/index.ts
+++ src/index.ts
@@ -16,3 +16,5 @@ export * from './events/base-publisher';
 export * from './events/ticket-created-event';
 export * from './events/ticket-updated-event';
 export * from './events/types/order-status';
+export * from './events/order-cancelled-event';
+export * from './events/order-created-event';
